587150e7e16d899fca7f715886ef1bc8efd4f2b4
teja7569 <tejavasam21@gmail.com>
2026-01-10 16:02:29 +0530

    Reorder Vite config properties

--- Typing_speed_test/vite.config.js
+++ Typing_speed_test/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Typing_speed_test/",
   plugins: [react()],
+  base: '/Typing_speed_test/'
 })
